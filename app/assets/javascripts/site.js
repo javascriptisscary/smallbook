770d655ec070c47b36cf0fe1306ca581aadeb0cf
Kenneth <crazedpokerpro@yahoo.com>
2015-12-29 20:54:40 -0500

    customized datepicker

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,13 @@
 $(document).on('ready page:load', function () {
 
-  console.log("yes i'm running biatch");
+  
   
   //fade notices and alerts
   $('.notice, .alert').fadeOut(5000); 
 
 
   //datepicker for adding birthday
-  $( "#datepicker" ).datepicker({ format: 'mm/dd/yyyy'})
+  $( "#datepicker" ).datepicker({ changeYear:true, yearRange: "1940:2005", format: 'mm/dd/yyyy',  defaultDate: "01/01/1984"});
 
   
 
