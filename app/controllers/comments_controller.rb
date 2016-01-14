a9d9f25a7e7d425c99a2cd6107f6c3c6d1d8a1f4
Kenneth <crazedpokerpro@yahoo.com>
2016-01-14 16:57:10 -0500

    more renders for cleaner code, more fixes for mobile css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    
+    @comment= Comment.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Comment erased."
+    redirect_to :back
   end
 
 
