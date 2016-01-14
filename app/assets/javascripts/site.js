3cc2bc58ecd6f112d992db04304bc92217cd3341
Kenneth <crazedpokerpro@yahoo.com>
2016-01-14 18:12:30 -0500

    js for camera icon location per dynamic photos

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -10,6 +10,12 @@ $(document).on('ready page:load', function () {
   $( "#datepicker" ).datepicker({ changeMonth: true, changeYear:true, yearRange: "1940:2005", dateFormat: 'yy-mm-dd',  defaultDate: '1980-01-01'});
 
   
+// add a class tomake height and width of photo div exact same size as dynamic image from paperclip
+var image_width = $('.profile-image').width();
+var image_height = $('.profile-image').height();
+
+$('#photo').addClass('modwidth').width(image_width);
+ $('#photo').addClass('modheight').height(image_height);
 
 
 
