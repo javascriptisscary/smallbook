fc3d65218579a450592536989aec62aa8d335982
Kenneth <crazedpokerpro@yahoo.com>
2016-01-14 20:41:41 -0500

    change date field to text for cross browser datepicker

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -5,9 +5,16 @@ $(document).on('ready page:load', function () {
   //fade notices and alerts
   $('.notice, .alert').fadeOut(5000); 
 
-
-  //datepicker for adding birthday
+  
+  
+   
+         //datepicker for adding birthday
   $( "#datepicker" ).datepicker({ changeMonth: true, changeYear:true, yearRange: "1940:2005", dateFormat: 'yy-mm-dd',  defaultDate: '1980-01-01'});
+    
+ 
+
+
+  
 
   
 // add a class tomake height and width of photo div exact same size as dynamic image from paperclip
